d4a857d16abbc9f7099bd3c795eed3d8a0b3faae
AA22_Gi <gibert22@mail.ru>
2024-09-08 18:12:43 +0700

    Добавлена переменная is_frozen И декораторы @unittest.skipIf() к методам класса

--- _12_supporting_the_development_cycle/_01_runner_test.py
+++ _12_supporting_the_development_cycle/_01_runner_test.py
@@ -3,18 +3,23 @@ import unittest
 
 
 class RunnerTest(unittest.TestCase):
+    is_frozen = False
+
+    @unittest.skipIf(is_frozen, 'Тесты в этом кейсе заморожены')
     def test_walk(self):
         alex = runner.Runner('Alex')
         for _ in range(10):
             alex.walk()
         self.assertEqual(alex.distance, 50)
 
+    @unittest.skipIf(is_frozen, 'Тесты в этом кейсе заморожены')
     def test_run(self):
         oleg = runner.Runner('Oleg')
         for _ in range(10):
             oleg.run()
         self.assertEqual(oleg.distance, 100)
 
+    @unittest.skipIf(is_frozen, 'Тесты в этом кейсе заморожены')
     def test_challenge(self):
         vica = runner.Runner('Vica')
         marina = runner.Runner('Marina')
