6b676efee17989a595741f880d95c61d9b456120
AA22_Gi <gibert22@mail.ru>
2024-09-11 00:20:37 +0700

    В функцию start_tournament await sleep(power) перемещена в начало цикла for

--- _13_asynchronous_programming/_01_asynchrony.py
+++ _13_asynchronous_programming/_01_asynchrony.py
@@ -5,8 +5,8 @@ from asyncio import sleep
 async def start_strongman(name, power):
     print(f'Силач {name} начал соревнования.')
     for ball_number in range(1, 6):
-        print(f'Силач {name} поднял {ball_number}')
         await sleep(power)
+        print(f'Силач {name} поднял {ball_number}')
     print(f'Силач {name} закончил соревнования.')
 
 
