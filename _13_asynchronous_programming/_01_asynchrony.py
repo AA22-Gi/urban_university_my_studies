6662da284a6fa21024f2913d2a62f0c7c8b327c2
AA22_Gi <gibert22@mail.ru>
2024-09-11 00:11:54 +0700

    В функцию start_tournament для задач добавлен await

--- _13_asynchronous_programming/_01_asynchrony.py
+++ _13_asynchronous_programming/_01_asynchrony.py
@@ -14,7 +14,9 @@ async def start_tournament():
     task_1 = asyncio.create_task(start_strongman('Pasha', 3))
     task_2 = asyncio.create_task(start_strongman('Denis', 4))
     task_3 = asyncio.create_task(start_strongman('Apollon', 5))
+    await task_1
+    await task_2
+    await task_3
 
 
 asyncio.run(start_tournament())
-
