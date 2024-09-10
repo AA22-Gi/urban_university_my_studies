212edf3f2e9ffab603bfe6a4825ce49c7944f765
AA22_Gi <gibert22@mail.ru>
2024-09-10 23:56:10 +0700

    Добавлена библиотека asyncio В функцию start_strongman добавлена информация о начале, окончании и процессе работы силача

--- _13_asynchronous_programming/_01_asynchrony.py
+++ _13_asynchronous_programming/_01_asynchrony.py
@@ -1,4 +1,10 @@
+import asyncio
+from asyncio import sleep
 
 
 async def start_strongman(name, power):
-    pass
+    print(f'Силач {name} начал соревнования.')
+    for ball_number in range(1, 6):
+        print(f'Силач {name} поднял {ball_number}')
+        sleep(ball_number)
+    print(f'Силач {name} закончил соревнования.')
