3710130d0cc3f222c045e6f2b0e5dafb2e8369eb
AA22_Gi <gibert22@mail.ru>
2024-09-11 00:03:27 +0700

    В функцию start_strongman добавлен await

--- _13_asynchronous_programming/_01_asynchrony.py
+++ _13_asynchronous_programming/_01_asynchrony.py
@@ -6,7 +6,7 @@ async def start_strongman(name, power):
     print(f'Силач {name} начал соревнования.')
     for ball_number in range(1, 6):
         print(f'Силач {name} поднял {ball_number}')
-        sleep(power)
+        await sleep(power)
     print(f'Силач {name} закончил соревнования.')
 
 
