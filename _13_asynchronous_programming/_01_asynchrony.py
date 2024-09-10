0df845aed11ce4d02d00a2bb836cd0df450f3e51
AA22_Gi <gibert22@mail.ru>
2024-09-10 23:58:06 +0700

    Добавлена функция start_tournament()

--- _13_asynchronous_programming/_01_asynchrony.py
+++ _13_asynchronous_programming/_01_asynchrony.py
@@ -6,5 +6,9 @@ async def start_strongman(name, power):
     print(f'Силач {name} начал соревнования.')
     for ball_number in range(1, 6):
         print(f'Силач {name} поднял {ball_number}')
-        sleep(ball_number)
+        sleep(power)
     print(f'Силач {name} закончил соревнования.')
+
+
+async def start_tournament():
+    pass
