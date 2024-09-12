d8e3ed3a1ddbaa20c93346358566d668f2d05379
AA22_Gi <gibert22@mail.ru>
2024-09-12 22:04:56 +0700

    Добавлены асинхронные функции start и all_massages

--- _13_asynchronous_programming/_02_handlers.py
+++ _13_asynchronous_programming/_02_handlers.py
@@ -7,5 +7,16 @@ api = API
 bot = Bot(token=api)
 dp = Dispatcher(bot, storage=MemoryStorage())
 
+
+@dp.message_handler()
+async def start(message):
+    pass
+
+
+@dp.message_handler()
+async def all_massages(message):
+    pass
+
+
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True)
