f337dcf84a9d37f47495931b7bf1c5f1cde596b8
AA22_Gi <gibert22@mail.ru>
2024-09-12 21:43:48 +0700

    Добавлен api, bot, dp

--- _13_asynchronous_programming/_02_handlers.py
+++ _13_asynchronous_programming/_02_handlers.py
@@ -2,7 +2,6 @@ from aiogram import Bot, Dispatcher, executor, types
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 import asyncio
 
-
-
-
-
+api = '7431730886:AAG_OfnRd9Xb6ex7hZXUZTppXz-1Zm0XDek'
+bot = Bot(token=api)
+dp = Dispatcher(bot, storage=MemoryStorage())
