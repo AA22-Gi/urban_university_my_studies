8d18b0b6f6ec6e5de43ddc5ba62562052f746d3e
AA22_Gi <gibert22@mail.ru>
2024-09-12 21:47:26 +0700

    Добавлен if __name__ == '__main__'

--- _13_asynchronous_programming/_02_handlers.py
+++ _13_asynchronous_programming/_02_handlers.py
@@ -5,3 +5,6 @@ import asyncio
 api = '7431730886:AAG_OfnRd9Xb6ex7hZXUZTppXz-1Zm0XDek'
 bot = Bot(token=api)
 dp = Dispatcher(bot, storage=MemoryStorage())
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
