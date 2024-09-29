d4f4772dc2f4c7a3b56adf41897dd0158d177395
AA22_Gi <gibert22@mail.ru>
2024-09-29 18:12:18 +0700

    В хендлеры set_growth, set_weight, send_calories добавлен блок 'if' - 'else' В хендере перемещен с 70 на 23 строку и в нем изменен текст

--- _13_asynchronous_programming/_04_state.py
+++ _13_asynchronous_programming/_04_state.py
@@ -1,10 +1,7 @@
 from aiogram import Bot, Dispatcher, executor, types
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
-from aiogram.dispatcher.filters.state import State, StatesGroup
 import asyncio
-
 from pyexpat.errors import messages
-
 from api_bot import API
 from aiogram.dispatcher.filters.state import State, StatesGroup
 
@@ -19,42 +16,55 @@ class UserState(StatesGroup):
     weight = State()
 
 
-@dp.message_handler(message=['Calories'])
-async def set_age(message: types.Message):
-    await message.answer('Введите свой возраст:')
+@dp.message_handler(commands=['start'])
+async def start(message):
+    await message.answer('Привет! Я бот помогающий твоему здоровью.\n'
+                         'Если Вы хотите рассчитать суточную норму калорий введите "Calories"')
+
+
+@dp.message_handler(text=['Calories'])
+async def set_age(message):
+    await message.answer('Введите свой возраст: ')
     await UserState.age.set()
 
 
 @dp.message_handler(state=UserState.age)
 async def set_growth(message, state):
-    await state.update_data(age=message.text)
-    await message.answer('Введите свой рост: ')
-    await UserState.growth.set()
+    if message.text.isdigit():
+        await state.update_data(age=message.text)
+        await message.answer('Введите свой рост: ')
+        await UserState.growth.set()
+    else:
+        await message.answer('Возраст должен быть целым числом.\n'
+                             'Попробуйте ещё раз: ')
 
 
 @dp.message_handler(state=UserState.growth)
 async def set_weight(message, state):
-    await state.update_data(weight=message.text)
-    await message.answer('Введите свой вес: ')
-    await UserState.weight.set()
+    if message.text.isdigit():
+        await state.update_data(weight=message.text)
+        await message.answer('Введите свой вес: ')
+        await UserState.weight.set()
+    else:
+        await message.answer('Рост должен быть целым числом.\n'
+                             'Попробуйте ещё раз: ')
 
 
 @dp.message_handler(state=UserState.weight)
 async def send_calories(message, state):
-    await state.update_data(calories=message.text)
-    data = state.get_data()
-    age = data['age']
-    growth = data['growth']
-    weight = data['weight']
+    if message.text.isdigit():
+        await state.update_data(growth=message.text)
+    else:
+        await message.answer('Вес должен быть целым числом.\n'
+                             'Попробуйте ещё раз: ')
+    data = await state.get_data()
+    age = int(data['age'])
+    growth = int(data['growth'])
+    weight = int(data['weight'])
     # упрощенна формула Миффлина для мужчин
     calories = (10 * weight) + (6.25 * growth) - (5 * age) + 5
-    await message.answer(f'Ваша норма калорий: {calories:.2f} ккал')
+    await message.answer(f'Ваша норма калорий: {calories} ккал')
     await state.finish()
-
-
-@dp.message_handler(commands=['start'])
-async def start(message):
-    await message.answer('Привет! Я бот помогающий твоему здоровью.')
 
 
 @dp.message_handler()
