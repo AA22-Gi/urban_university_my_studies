d2d8b9a74e006e8f1293cbccd486a780c7c3855f
AA22_Gi <gibert22@mail.ru>
2024-09-30 20:10:02 +0700

    Добавлен cursor.execute Создаем табличку с полями

--- _14_library_for_working_with_data/_01_database_creating.py
+++ _14_library_for_working_with_data/_01_database_creating.py
@@ -1,5 +1,14 @@
 import sqlite3
 
 connection = sqlite3.connect("not_telegram.db")
-
 cursor = connection.cursor()
+
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS Users(
+id INTEGER PRIMARY KEY,
+username TEXT NOT NULL,
+email TEXT NOT NULL,
+age INTEGER,
+balance INTEGER NOT NULL
+''')
+
