bc0b6b6d65b7eb58c441fc35ce7072aadb5f39fa
AA22_Gi <gibert22@mail.ru>
2024-10-01 01:28:32 +0700

    Снял комментирование кода

--- _14_library_for_working_with_data/_01_database_creating.py
+++ _14_library_for_working_with_data/_01_database_creating.py
@@ -13,16 +13,16 @@ balance INTEGER NOT NULL
 )
 ''')
 
-# for i in range(1, 11):
-#     cursor.execute('INSERT INTO Users(username, email, age, balance) VALUES(?, ?, ?, ?)',
-#                    (f'User{i}', f'example{i}@gmail.com', f'{i * 10}', '1000'))
+for i in range(1, 11):
+    cursor.execute('INSERT INTO Users(username, email, age, balance) VALUES(?, ?, ?, ?)',
+                   (f'User{i}', f'example{i}@gmail.com', f'{i * 10}', '1000'))
 
-# for i in range(1, 11, 2):
-#     cursor.execute('UPDATE Users SET balance = ? WHERE id = ?',
-#                    (500, i))
+for i in range(1, 11, 2):
+    cursor.execute('UPDATE Users SET balance = ? WHERE id = ?',
+                   (500, i))
 
-# for i in range(1, 11, 3):
-#     cursor.execute('DELETE FROM Users WHERE id = ?', (i,))
+for i in range(1, 11, 3):
+    cursor.execute('DELETE FROM Users WHERE id = ?', (i,))
 
 cursor.execute('SELECT username, email, age, balance FROM Users WHERE age != ?', (60,))
 users = cursor.fetchall()
