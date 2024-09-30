2490492bbcf2fcebbaf90d45609fce74662c1347
AA22_Gi <gibert22@mail.ru>
2024-09-30 21:58:20 +0700

    Изменен print, чтобы соответствовать условию задачи

--- _14_library_for_working_with_data/_01_database_creating.py
+++ _14_library_for_working_with_data/_01_database_creating.py
@@ -28,7 +28,7 @@ cursor.execute('SELECT username, email, age, balance FROM Users WHERE age != ?',
 users = cursor.fetchall()
 
 for user in users:
-    print(user)
+    print(f'Имя: {user[0]} | Почта: {user[1]} | Возраст: {user[2]} | Баланс: {user[3]}')
 
 connection.commit()
 connection.close()
