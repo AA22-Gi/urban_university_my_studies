d6381506bd3f4f5f8da9b9f48df1650cf28cafb5
AA22_Gi <gibert22@mail.ru>
2024-10-01 09:48:03 +0700

    Удаление пользователя под индексом 6

--- _14_library_for_working_with_data/_02_sql_queries.py
+++ _14_library_for_working_with_data/_02_sql_queries.py
@@ -13,22 +13,24 @@ balance INTEGER NOT NULL
 )
 ''')
 
-for i in range(1, 11):
-    cursor.execute('INSERT INTO Users(username, email, age, balance) VALUES(?, ?, ?, ?)',
-                   (f'User{i}', f'example{i}@gmail.com', f'{i * 10}', '1000'))
-
-for i in range(1, 11, 2):
-    cursor.execute('UPDATE Users SET balance = ? WHERE id = ?',
-                   (500, i))
-
-for i in range(1, 11, 3):
-    cursor.execute('DELETE FROM Users WHERE id = ?', (i,))
-
+# for i in range(1, 11):
+#     cursor.execute('INSERT INTO Users(username, email, age, balance) VALUES(?, ?, ?, ?)',
+#                    (f'User{i}', f'example{i}@gmail.com', f'{i * 10}', '1000'))
+#
+# for i in range(1, 11, 2):
+#     cursor.execute('UPDATE Users SET balance = ? WHERE id = ?',
+#                    (500, i))
+#
+# for i in range(1, 11, 3):
+#     cursor.execute('DELETE FROM Users WHERE id = ?', (i,))
+#
 cursor.execute('SELECT username, email, age, balance FROM Users WHERE age != ?', (60,))
 users = cursor.fetchall()
 
-for user in users:
-    print(f'Имя: {user[0]} | Почта: {user[1]} | Возраст: {user[2]} | Баланс: {user[3]}')
+
+cursor.execute('DELETE FROM Users WHERE id = ?', (6,))
+
+
 
 connection.commit()
 connection.close()
