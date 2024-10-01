97c5c5c921cbcd0f9c791d39809d8078de524727
AA22_Gi <gibert22@mail.ru>
2024-10-01 09:55:14 +0700

    Добавлены пустые строки и вывод на консоль

--- _14_library_for_working_with_data/_02_sql_queries.py
+++ _14_library_for_working_with_data/_02_sql_queries.py
@@ -24,15 +24,18 @@ balance INTEGER NOT NULL
 # for i in range(1, 11, 3):
 #     cursor.execute('DELETE FROM Users WHERE id = ?', (i,))
 #
-cursor.execute('SELECT username, email, age, balance FROM Users WHERE age != ?', (60,))
-users = cursor.fetchall()
+# cursor.execute('SELECT username, email, age, balance FROM Users WHERE age != ?', (60,))
+# users = cursor.fetchall()
 
 cursor.execute('DELETE FROM Users WHERE id = ?', (6,))
+
 cursor.execute('SELECT COUNT(*) FROM Users')
 total_users = cursor.fetchone()[0]
+
 cursor.execute('SELECT SUM(balance) FROM Users')
 all_balances = cursor.fetchone()[0]
 
+print(all_balances/total_users)
 
 connection.commit()
 connection.close()
