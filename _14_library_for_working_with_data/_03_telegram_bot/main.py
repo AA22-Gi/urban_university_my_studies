98e57f7f28a77bfc3ab812007e864a32324e3979
AA22_Gi <gibert22@mail.ru>
2024-10-02 22:47:28 +0700

    Дописаны обработчики товаров

--- _14_library_for_working_with_data/_03_telegram_bot/main.py
+++ _14_library_for_working_with_data/_03_telegram_bot/main.py
@@ -21,6 +21,11 @@ async def start(message):
     await message.answer(hello, reply_markup=start_kb)
 
 
+@dp.message_handler(text=['Информация'])
+async def info(message):
+    await message.answer(information)
+
+
 @dp.message_handler(text=['Рассчитать'])
 async def main_menu(message):
     await message.answer('Выберите опцию: ', reply_markup=catalog_kb)
@@ -65,14 +70,49 @@ async def send_calories(message, state):
         await state.update_data(growth=message.text)
     else:
         await message.answer(again)
+
     data = await state.get_data()
     age = int(data['age'])
     growth = int(data['growth'])
     weight = int(data['weight'])
+
     # упрощенна формула Миффлина для мужчин
     calories = (10 * weight) + (6.25 * growth) - (5 * age) + 5
     await message.answer(f'Ваша норма калорий: {calories} ккал')
     await state.finish()
+
+
+@dp.message_handler(text=['Купить'])
+async def prod(message):
+    await message.answer('Рады Вас видеть!', reply_markup=product_kb)
+
+
+@dp.callback_query_handler(text='shaker')
+async def prod_1(call):
+    with open('1.png', 'rb') as img:
+        await call.message.answer_photo(img, 'Шейкер')
+    await call.answer()
+
+
+@dp.callback_query_handler(text='jump_rope')
+async def prod_2(call):
+    with open('2.png', 'rb') as img:
+        await call.message.answer_photo(img, 'Скакалка')
+    await call.answer()
+
+
+@dp.callback_query_handler(text='elastic_band')
+async def prod_3(call):
+    with open('3.png', 'rb') as img:
+        await call.message.answer_photo(img, 'Фитнес-резинки')
+    await call.answer()
+
+
+@dp.callback_query_handler(text='dumbbells')
+async def prod_4(call):
+    with open('4.png', 'rb') as img:
+        await call.message.answer_photo(img, 'Гантели')
+    await call.answer()
 
 
 @dp.message_handler()
