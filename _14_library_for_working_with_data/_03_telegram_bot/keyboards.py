953f651dc1d586616618f8b24e440ddb4c1360fa
AA22_Gi <gibert22@mail.ru>
2024-10-02 10:07:08 +0700

    создан модуль keyboards.py
    Из aiogram.types import ReplyKeyboardMarkup, KeyboardButton
    и InlineKeyboardMarkup, InlineKeyboardButton

--- _14_library_for_working_with_data/_03_telegram_bot/keyboards.py
+++ _14_library_for_working_with_data/_03_telegram_bot/keyboards.py
@@ -2,24 +2,3 @@ from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 
-start_kb = ReplyKeyboardMarkup(
-    keyboard=[
-        [
-            KeyboardButton(text='Рассчитать'),
-            KeyboardButton(text='Информация')
-        ]
-    ], resize_keyboard=True
-)
-
-info_kb = InlineKeyboardMarkup(
-    inline_keyboard=[
-        [InlineKeyboardButton(text='Рассчитать норму калорий', callback_data='calories')],
-        [InlineKeyboardButton(text='Формулы расчёта', callback_data='formulas')],
-    ]
-)
-
-# buy_kb = InlineKeyboardMarkup(
-#     inline_keyboard=[
-#         [InlineKeyboardButton(text='Купить', url='https://yandex.ru')]
-#     ]
-# )
