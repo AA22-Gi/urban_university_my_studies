7bb3a4a70d96bf9a883ddcb64fe13b9836a7b607
AA22_Gi <gibert22@mail.ru>
2024-10-02 07:17:53 +0700

    Добавлена клавиатура start_kb = ReplyKeyboardMarkup()

--- _14_library_for_working_with_data/_03_telegram_bot/keyboards.py
+++ _14_library_for_working_with_data/_03_telegram_bot/keyboards.py
@@ -1,1 +1,11 @@
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+
+
+start_kb = ReplyKeyboardMarkup(
+    keyboard=[
+        [
+            KeyboardButton(text='Стоимость'),
+            KeyboardButton(text='О нас')
+        ]
+    ], resize_keyboard=True
+)
