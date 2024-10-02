01c00b384ed50718b8f08d4c257e0d42ac1a813a
AA22_Gi <gibert22@mail.ru>
2024-10-02 10:17:40 +0700

    Созданы клавиатуры

--- _14_library_for_working_with_data/_03_telegram_bot/keyboards.py
+++ _14_library_for_working_with_data/_03_telegram_bot/keyboards.py
@@ -2,3 +2,19 @@ from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 
+start_kb = ReplyKeyboardMarkup(
+    keyboard=[
+        [
+            KeyboardButton(text='Рассчитать'),
+            KeyboardButton(text='Информация')
+        ]
+    ], resize_keyboard=True
+)
+
+catalog_kb = InlineKeyboardMarkup(
+    inline_keyboard=[
+        [InlineKeyboardButton(text='Рассчитать норму калорий', callback_data='calories')],
+        [InlineKeyboardButton(text='Формулы расчёта', callback_data='formulas')],
+    ]
+)
+
