598d17ccdfa9242aea4aca7ea52a286a6fc64108
AA22_Gi <gibert22@mail.ru>
2024-10-02 23:37:21 +0700

    В клавиатуре product_kb изменена callback_data

--- _14_library_for_working_with_data/_03_telegram_bot/keyboards.py
+++ _14_library_for_working_with_data/_03_telegram_bot/keyboards.py
@@ -20,9 +20,9 @@ catalog_kb = InlineKeyboardMarkup(
 
 product_kb = InlineKeyboardMarkup(
     inline_keyboard=[
-        [InlineKeyboardButton(text='Шейкер', callback_data='shaker')],
-        [InlineKeyboardButton(text='Скакалка', callback_data='jump_rope')],
-        [InlineKeyboardButton(text='Фитнес-резинки', callback_data='elastic_band')],
-        [InlineKeyboardButton(text='Гантели', callback_data='dumbbells')]
+        [InlineKeyboardButton(text='Шейкер', callback_data='product_buying')],
+        [InlineKeyboardButton(text='Скакалка', callback_data='product_buying')],
+        [InlineKeyboardButton(text='Фитнес-резинки', callback_data='product_buying')],
+        [InlineKeyboardButton(text='Гантели', callback_data='product_buying')]
     ]
 )
