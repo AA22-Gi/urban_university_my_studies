6da5562d8cd7b947be2f924ebd7a144b3b995190
AA22_Gi <gibert22@mail.ru>
2024-10-02 08:27:56 +0700

    Добавлена клавиатура catalog_kb и buy_kb

--- _14_library_for_working_with_data/_03_telegram_bot/keyboards.py
+++ _14_library_for_working_with_data/_03_telegram_bot/keyboards.py
@@ -9,3 +9,18 @@ start_kb = ReplyKeyboardMarkup(
         ]
     ], resize_keyboard=True
 )
+
+catalog_kb = InlineKeyboardMarkup(
+    inline_keyboard=[
+        [InlineKeyboardButton(text='Средняя игра', callback_data='medium')],
+        [InlineKeyboardButton(text='Большая игра', callback_data='big')],
+        [InlineKeyboardButton(text='Очень большая игра', callback_data='mega')],
+        [InlineKeyboardButton(text='Другие предложения', callback_data='other')],
+    ]
+)
+
+buy_kb = InlineKeyboardMarkup(
+    inline_keyboard=[
+        [InlineKeyboardButton(text='Купить', url='https://yandex.ru')]
+    ]
+)
