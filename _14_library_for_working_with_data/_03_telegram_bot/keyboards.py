8860681453995662c75cdfb2a93eda92948c7630
AA22_Gi <gibert22@mail.ru>
2024-10-02 22:47:51 +0700

    Добавлена клавиатура product_kb

--- _14_library_for_working_with_data/_03_telegram_bot/keyboards.py
+++ _14_library_for_working_with_data/_03_telegram_bot/keyboards.py
@@ -1,13 +1,13 @@
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
-
 start_kb = ReplyKeyboardMarkup(
     keyboard=[
         [
             KeyboardButton(text='Рассчитать'),
             KeyboardButton(text='Информация')
-        ]
+        ],
+        [KeyboardButton(text='Купить')]
     ], resize_keyboard=True
 )
 
@@ -18,3 +18,11 @@ catalog_kb = InlineKeyboardMarkup(
     ]
 )
 
+product_kb = InlineKeyboardMarkup(
+    inline_keyboard=[
+        [InlineKeyboardButton(text='Шейкер', callback_data='shaker')],
+        [InlineKeyboardButton(text='Скакалка', callback_data='jump_rope')],
+        [InlineKeyboardButton(text='Фитнес-резинки', callback_data='elastic_band')],
+        [InlineKeyboardButton(text='Гантели', callback_data='dumbbells')]
+    ]
+)
