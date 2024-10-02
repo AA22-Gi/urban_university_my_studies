8d06edf2490462031f88cd68e325fdd2ccddb723
AA22_Gi <gibert22@mail.ru>
2024-10-02 07:36:57 +0700

    Добавлены три хендлера start, info, price

--- _14_library_for_working_with_data/_03_telegram_bot/main.py
+++ _14_library_for_working_with_data/_03_telegram_bot/main.py
@@ -1,19 +1,30 @@
 import logging
 
-from aiogram import Bot, Dispatcher, executor
+from aiogram import Bot, Dispatcher, executor, types
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 
 from config import *
 from keyboards import *
 import texts
 
-
 logging.basicConfig(level=logging.INFO)
 
 bot = Bot(token=API)
-db = Dispatcher(bot, storage=MemoryStorage())
+dp = Dispatcher(bot, storage=MemoryStorage())
 
+
+@dp.message_handler(commands=['start'])
+async def start(message):
+    await message.answer(texts.start, reply_markup=start_kb)
+
+@dp.message_handler(texts =['О нас'])
+async def info(message):
+    await message.answer(texts.about, reply_markup=start_kb)
+
+@dp.message_handler(texts =['Стоимость'])
+async def price(message):
+    await message.answer('Что Вас интересует? ', reply_markup=catalog_kb)
 
 
 if __name__ == '__main__':
-    executor.start_polling(db, skip_updates=True)
+    executor.start_polling(dp, skip_updates=True)
