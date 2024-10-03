7a9c6411f7d4c33940a4828b6d3dc4ed40c9890e
AA22_Gi <gibert22@mail.ru>
2024-10-03 17:52:01 +0700

    Модуль с хендлерами скопирован из директории _03_telegram_bot

---
+++ _14_library_for_working_with_data/_04_telegram_bot/main.py
@@ -0,0 +1,124 @@
+import asyncio
+import logging
+
+from aiogram import Bot, Dispatcher, executor, types
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.dispatcher.filters.state import State, StatesGroup
+
+from api_bot import API
+from keyboards import *
+from texst import *
+
+logging.basicConfig(level=logging.INFO)
+
+api = API
+bot = Bot(token=api)
+dp = Dispatcher(bot, storage=MemoryStorage())
+
+
+class UserState(StatesGroup):
+    age = State()
+    growth = State()
+    weight = State()
+
+
+@dp.message_handler(commands=['start'])
+async def start(message):
+    await message.answer(hello, reply_markup=start_kb)
+
+
+@dp.message_handler(text=['Информация'])
+async def info(message):
+    await message.answer(information)
+
+
+@dp.message_handler(text=['Рассчитать'])
+async def main_menu(message):
+    await message.answer('Выберите опцию: ', reply_markup=catalog_kb)
+
+
+@dp.callback_query_handler(text='formulas')
+async def get_formulas(call):
+    await call.message.answer(formula)
+    await call.answer()
+
+
+@dp.callback_query_handler(text=['calories'])
+async def set_age(call):
+    await call.message.answer('Введите свой возраст: ')
+    await call.answer()
+    await UserState.age.set()
+
+
+@dp.message_handler(state=UserState.age)
+async def set_growth(message, state):
+    if message.text.isdigit():
+        await state.update_data(age=message.text)
+        await message.answer('Введите свой рост: ')
+        await UserState.growth.set()
+    else:
+        await message.answer(again)
+
+
+@dp.message_handler(state=UserState.growth)
+async def set_weight(message, state):
+    if message.text.isdigit():
+        await state.update_data(weight=message.text)
+        await message.answer('Введите свой вес: ')
+        await UserState.weight.set()
+    else:
+        await message.answer(again)
+
+
+@dp.message_handler(state=UserState.weight)
+async def send_calories(message, state):
+    if message.text.isdigit():
+        await state.update_data(growth=message.text)
+    else:
+        await message.answer(again)
+
+    data = await state.get_data()
+    age = int(data['age'])
+    growth = int(data['growth'])
+    weight = int(data['weight'])
+
+    # упрощенна формула Миффлина для мужчин
+    calories = (10 * weight) + (6.25 * growth) - (5 * age) + 5
+    await message.answer(f'Ваша норма калорий: {calories} ккал')
+    await state.finish()
+
+
+@dp.message_handler(text=['Купить'])
+async def get_buying_list(message):
+    await message.answer(product_1)
+    with open('1.png', 'rb') as img:
+        await message.answer_photo(img)
+
+    await message.answer(product_2)
+    with open('2.png', 'rb') as img:
+        await message.answer_photo(img)
+
+    await message.answer(product_3)
+    with open('3.png', 'rb') as img:
+        await message.answer_photo(img)
+
+    await message.answer(product_4)
+    with open('4.png', 'rb') as img:
+        await message.answer_photo(img)
+
+    await message.answer('Выберите продукт для покупки:', reply_markup=product_kb)
+
+
+@dp.callback_query_handler(text='product_buying')
+async def send_confirm_message(call):
+    await call.message.answer('Вы успешно приобрели продукт!')
+    await call.answer()
+
+
+@dp.message_handler()
+async def all_massages(message):
+    await message.answer('Введите команду /start, чтобы начать общение.')
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
