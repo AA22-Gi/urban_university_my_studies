bb5aee3570303e1db886d8a7a0d94f8a55a56656
AA22_Gi <gibert22@mail.ru>
2024-10-04 10:46:12 +0700

    Клавиатура

---
+++ _14_library_for_working_with_data/_05_telegram_bot/keyboards.py
@@ -0,0 +1,28 @@
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+
+start_kb = ReplyKeyboardMarkup(
+    keyboard=[
+        [
+            KeyboardButton(text='Рассчитать'),
+            KeyboardButton(text='Информация')
+        ],
+        [KeyboardButton(text='Купить')]
+    ], resize_keyboard=True
+)
+
+catalog_kb = InlineKeyboardMarkup(
+    inline_keyboard=[
+        [InlineKeyboardButton(text='Рассчитать норму калорий', callback_data='calories')],
+        [InlineKeyboardButton(text='Формула расчёта', callback_data='formulas')],
+    ]
+)
+
+product_kb = InlineKeyboardMarkup(
+    inline_keyboard=[
+        [InlineKeyboardButton(text='Шейкер', callback_data='product_buying'),
+        InlineKeyboardButton(text='Скакалка', callback_data='product_buying'),
+        InlineKeyboardButton(text='Фитнес-резинки', callback_data='product_buying'),
+        InlineKeyboardButton(text='Гантели', callback_data='product_buying')]
+    ]
+)
