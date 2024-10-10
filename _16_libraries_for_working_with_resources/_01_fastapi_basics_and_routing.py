1c618c417d8da33eeee4bb6a5b0c9568b30b0f49
AA22_Gi <gibert22@mail.ru>
2024-10-11 00:54:44 +0700

    добавлена строка для запуска

--- _16_libraries_for_working_with_resources/_01_fastapi_basics_and_routing.py
+++ _16_libraries_for_working_with_resources/_01_fastapi_basics_and_routing.py
@@ -1,8 +1,10 @@
 from fastapi import FastAPI
 
 app = FastAPI()
+# строка для запуска через терминал
+command_to_run = 'python -m uvicorn _16_libraries_for_working_with_resources._01_fastapi_basics_and_routing:app'
+
 
 @app.get('/')
 async def main() -> str:
     return 'Главная страница'
-
