2b73e1b82ac20220ccd354c564a728ea92f7d89c
AA22_Gi <gibert22@mail.ru>
2024-10-11 00:59:20 +0700

    Написана функция _user_id

--- _16_libraries_for_working_with_resources/_01_fastapi_basics_and_routing.py
+++ _16_libraries_for_working_with_resources/_01_fastapi_basics_and_routing.py
@@ -9,6 +9,10 @@ command_to_run = 'python -m uvicorn _16_libraries_for_working_with_resources._01
 async def admin() -> str:
     return 'Вы вошли как администратор'
 
+@app.get('/user/{user_id}')
+async def _user_id(user_id: str = 'Alex') -> str:
+    return 'Вы вошли как пользователь № {user_id}'
+
 
 @app.get('/')
 async def main() -> str:
