22d4b98d29df1c376da837668c31ab209f308666
AA22_Gi <gibert22@mail.ru>
2024-10-12 10:05:19 +0700

    Написана функция create_user, которая выполняет post запрос

--- _16_libraries_for_working_with_resources/_03_CRUD_requests.py
+++ _16_libraries_for_working_with_resources/_03_CRUD_requests.py
@@ -3,6 +3,14 @@ from fastapi import FastAPI, Path
 app = FastAPI()
 users = {'1': 'Имя: Example, возраст: 18'}
 
+
 @app.get('/users')
 async def get_all_users() -> dict:
     return users
+
+
+@app.post('/user/{username}/{age}')
+async def create_user(username: str, age: int):
+    user_id = str(max(map(int, users.keys())) + 1)
+    users[user_id] = f'Имя: {username}, возраст: {age}'
+    return 'User {user_id} is registered'
