9dd9c351e3be7afd94cea3e176754a236cbb3941
AA22_Gi <gibert22@mail.ru>
2024-10-12 11:32:46 +0700

    Revert "Создана функция delete_user удаляющая пользователя из словаря если он есть."

--- _16_libraries_for_working_with_resources/_03_CRUD_requests.py
+++ _16_libraries_for_working_with_resources/_03_CRUD_requests.py
@@ -16,7 +16,7 @@ async def create_user(username: Annotated[str, Path(min_length=4, max_length=20,
                                                     example='UrbanUser')],
                       age: Annotated[int, Path(ge=12, le=70,
                                                description='Enter age',
-                                               example='24')]) -> str:
+                                               example='24')]):
     user_id = str(max(map(int, users.keys())) + 1)
     users[user_id] = f'Имя: {username}, возраст: {age}'
     return 'User {user_id} is registered'
@@ -29,18 +29,7 @@ async def update_user_value(user_id: str,
                                                           example='UrbanUser')],
                             age: Annotated[int, Path(ge=12, le=70,
                                                      description='Enter age',
-                                                     example='24')]) -> str:
+                                                     example='24')]):
     if user_id in users:
         users[user_id] = f'Имя: {username}, возраст: {age}'
     return f'The user {user_id} is registered'
-
-
-@app.delete('/user/{user_id}')
-async def delete_user(user_id: str) -> str:
-    if user_id in users:
-        username = users[user_id].split()[1][:-1]
-        del users[user_id]
-    return f'Пользователь {username} удален'
-
-
-command_to_run = 'python -m uvicorn _16_libraries_for_working_with_resources._03_CRUD_requests:app --reload'
