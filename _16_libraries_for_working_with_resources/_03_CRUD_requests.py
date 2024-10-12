85ffaf85a38d693ce6ec2b11d3ed226c56a2e233
AA22_Gi <gibert22@mail.ru>
2024-10-12 10:13:06 +0700

    В функцию create_user добавлена валидация

--- _16_libraries_for_working_with_resources/_03_CRUD_requests.py
+++ _16_libraries_for_working_with_resources/_03_CRUD_requests.py
@@ -11,7 +11,12 @@ async def get_all_users() -> dict:
 
 
 @app.post('/user/{username}/{age}')
-async def create_user(username: str, age: int):
+async def create_user(username: Annotated[str, Path(min_length=4, max_length=20,
+                                                    description='Enter username',
+                                                    example='UrbanUser')],
+                      age: Annotated[int, Path(ge=12, le=70,
+                                               description='Enter age',
+                                               example='24')]):
     user_id = str(max(map(int, users.keys())) + 1)
     users[user_id] = f'Имя: {username}, возраст: {age}'
     return 'User {user_id} is registered'
