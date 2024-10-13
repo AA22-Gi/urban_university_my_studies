f1a3efdffbefeacafa3f11abe69c6d29fb94bc17
AA22_Gi <gibert22@mail.ru>
2024-10-13 12:13:35 +0700

    Создан модуль _04_pydantic_data_models.py в который скопирован код из предыдущего модуля

---
+++ _16_libraries_for_working_with_resources/_04_pydantic_data_models.py
@@ -0,0 +1,46 @@
+from fastapi import FastAPI, Path
+from typing import Annotated
+
+app = FastAPI()
+users = {'1': 'Имя: Example, возраст: 18'}
+
+
+@app.get('/users')
+async def get_all_users() -> dict:
+    return users
+
+
+@app.post('/user/{username}/{age}')
+async def create_user(username: Annotated[str, Path(min_length=4, max_length=20,
+                                                    description='Enter username',
+                                                    example='UrbanUser')],
+                      age: Annotated[int, Path(ge=12, le=70,
+                                               description='Enter age',
+                                               example='24')]) -> str:
+    user_id = str(max(map(int, users.keys())) + 1)
+    users[user_id] = f'Имя: {username}, возраст: {age}'
+    return f'User {user_id} is registered'
+
+
+@app.put('/user/{user_id}/{username}/{age}')
+async def update_user_value(user_id: str,
+                            username: Annotated[str, Path(min_length=4, max_length=20,
+                                                          description='Enter username',
+                                                          example='UrbanUser')],
+                            age: Annotated[int, Path(ge=12, le=70,
+                                                     description='Enter age',
+                                                     example='24')]) -> str:
+    if user_id in users:
+        users[user_id] = f'Имя: {username}, возраст: {age}'
+        return f'The user {user_id} is registered'
+
+
+@app.delete('/user/{user_id}')
+async def delete_user(user_id: str) -> str:
+    if user_id in users:
+        username = users[user_id].split()[1][:-1]
+        del users[user_id]
+    return f'Пользователь {username} удален'
+
+
+command_to_run = 'python -m uvicorn _16_libraries_for_working_with_resources._03_CRUD_requests:app --reload'
