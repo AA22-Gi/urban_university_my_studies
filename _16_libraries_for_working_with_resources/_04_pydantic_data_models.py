2d73c33897e695d87d4890f167c6fa5bcf00a2b3
AA22_Gi <gibert22@mail.ru>
2024-10-13 13:17:56 +0700

    1) Изменена функция update_user_value: проходим циклом по пользователям и если индекс пользователя совпадает с переданным индексом меняем имя и возраст пользователя и возвращаем пользователя. Если такого пользователя нет, возвращаем ошибку 404
    2) Изменена функция delete_user:
    проходим циклом по users если индекс пользователя совпадает с переданным индексом удаляем пользователя. Если нет - возвращаем ошибку 404

--- _16_libraries_for_working_with_resources/_04_pydantic_data_models.py
+++ _16_libraries_for_working_with_resources/_04_pydantic_data_models.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Path
+from fastapi import FastAPI, Path, HTTPException
 from typing import Annotated
 from pydantic import BaseModel
 
@@ -37,18 +37,27 @@ async def update_user_value(user_id: str,
                                                           example='UrbanUser')],
                             age: Annotated[int, Path(ge=12, le=70,
                                                      description='Enter age',
-                                                     example='24')]) -> str:
-    if user_id in users:
-        users[user_id] = f'Имя: {username}, возраст: {age}'
-        return f'The user {user_id} is registered'
+                                                     example='24')]) -> User:
+
+    for user in users:
+        if user.id == user_id:
+            user.username = username
+            user.age = age
+            return user
+
+    raise HTTPException(status_code=404, detail='User was not found')
 
 
 @app.delete('/user/{user_id}')
-async def delete_user(user_id: str) -> str:
-    if user_id in users:
-        username = users[user_id].split()[1][:-1]
-        del users[user_id]
-    return f'Пользователь {username} удален'
+async def delete_user(user_id: str) -> User:
+    for user in users:
+        if user.id == user_id:
+            users.remove(user)
+            return user
+
+    raise HTTPException(status_code=404, detail='User was not found')
+
+
 
 
 command_to_run = 'python -m uvicorn _16_libraries_for_working_with_resources._03_CRUD_requests:app --reload'
