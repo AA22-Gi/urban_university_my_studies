b65b35cc8b10ca1e15e2d967896571173298286c
AA22_Gi <gibert22@mail.ru>
2024-10-18 23:35:25 +0700

    Задачи 1-го модуля

---
+++ _01_basic_data_structures/Homework_1 (Strings and String Indexing).py
@@ -0,0 +1,9 @@
+string = 'это моя строка'
+new_string = 'это новая строка'
+
+print(string[0])
+print(string[-1])
+print(string[2:5])
+print(string[::-1])
+print(len(string))
+print(new_string + string)
