b370705e148d98643daea7f31086fae01ea9cde0
AA22_Gi <gibert22@mail.ru>
2024-10-20 13:07:12 +0700

    Добавлен блок if __name__ == '__main__' Импортирован uvicorn Добавлен фрагмент кода для запуска

--- _17_library_for_working_with_resources2/app/main.py
+++ _17_library_for_working_with_resources2/app/main.py
@@ -1,3 +1,4 @@
+import uvicorn
 from fastapi import FastAPI
 from routers import task, user
 
@@ -11,3 +12,7 @@ async def welcome():
 
 app.include_router(task.router)
 app.include_router(user.router)
+
+
+if __name__ == '__main__':
+    uvicorn.run(app, host="127.0.0.1", port=8000)
