2b6f31210ce41c108ee8eca7f116ad70397356ee
AA22_Gi <gibert22@mail.ru>
2024-10-20 12:48:05 +0700

    Созданы классы наследуемые от BaseModel: CreateUser, UpdateUser, CreateTask, UpdateTask

--- _17_library_for_working_with_resources2/app/schemas.py
+++ _17_library_for_working_with_resources2/app/schemas.py
@@ -1,2 +1,17 @@
 from pydantic import BaseModel
 
+
+class CreateUser(BaseModel):
+    pass
+
+
+class UpdateUser(BaseModel):
+    pass
+
+
+class CreateTask(BaseModel):
+    pass
+
+
+class UpdateTask(BaseModel):
+    pass
