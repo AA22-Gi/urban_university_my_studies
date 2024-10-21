11600f5947b3d2ad02ba08aa5d01efdb2892a110
AA22_Gi <gibert22@mail.ru>
2024-10-21 22:03:07 +0700

    Импортированы необходимые модули Создан класс User

---
+++ _17_library_for_working_with_resources2/app/models/user.py
@@ -0,0 +1,15 @@
+from _17_library_for_working_with_resources2.app.backend.db import Base
+from sqlalchemy import Column, Integer, String, Boolean, ForeignKey
+from sqlalchemy.orm import relationship
+
+class User(Base):
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String)
+    firstname = Column(String)
+    lastname = Column(String)
+    age = Column(Integer)
+    slug = Column(String, unique=True, index=True)
+
+    tasks = ...
